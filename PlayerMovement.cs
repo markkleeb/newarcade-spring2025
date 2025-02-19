e8de21e5285e1240b2ccd37deb0a3e2a7a2f36bf
Mark Kleback <kleeb930@gmail.com>
2025-02-19 15:26:16 -0500

    Week 4 Top-Down Movement

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -2,63 +2,28 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Animator animator;
 
-    public KeyCode moveLeft;
-    public KeyCode moveRight;
-    public KeyCode jump;
-
-    public float jumpForce = 10.0f;
-    public float speed = 20.0f;
-
-    private Rigidbody2D theresaRB;
-    private SpriteRenderer theresaSR;
-    private bool facingLeft = true;
+    private Vector2 movement;
+    public float speed = 10.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        theresaSR = gameObject.GetComponent<SpriteRenderer>();
-        theresaRB = gameObject.GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
-
     {
 
-        Debug.Log(facingLeft);
-       // if (Input.GetKey(moveLeft))
-       if(Input.GetAxisRaw("Horizontal") < 0)
-        {
-            transform.Translate(Vector2.left * Time.deltaTime* speed);
-            facingLeft = true;
-        }
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
 
-        //if (Input.GetKey(moveRight))
-        if (Input.GetAxisRaw("Horizontal") > 0)
-        {
-            transform.Translate(Vector2.right * Time.deltaTime* speed);
-            facingLeft = false;
-        }
+        animator.SetFloat("Hz", movement.x);
+        animator.SetFloat("Vt", movement.y);
 
-        if (Input.GetKeyDown(jump))
-        {
-            theresaRB.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-        }
-
-        if (facingLeft)
-        {
-            theresaSR.flipX = false;
-        }
-        else
-        {
-            theresaSR.flipX = true;
-        }
-
-
+        transform.Translate(movement * speed * Time.deltaTime);
         
-
-
     }
 }
